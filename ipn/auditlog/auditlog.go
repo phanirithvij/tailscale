584e892b70e6553ce90c105f481399ae83abaaeb
Nick Khyl <nickk@tailscale.com>
2025-03-24 15:40:12 -0500

    cmd/tailscaled,ipn/{auditlog,ipnlocal},tsd: omit auditlog unless explicitly imported
    In this PR, we update ipnlocal.LocalBackend to allow registering callbacks for control client creation
    and profile changes. We also allow to register ipnauth.AuditLogFunc to be called when an auditable
    action is attempted.
    
    We then use all this to invert the dependency between the auditlog and ipnlocal packages and make
    the auditlog functionality optional, where it only registers its callbacks via ipnlocal-provided hooks
    when the auditlog package is imported.
    
    We then underscore-import it when building tailscaled for Windows, and we'll explicitly
    import it when building xcode/ipn-go-bridge for macOS. Since there's no default log-store
    location for macOS, we'll also need to call auditlog.SetStoreFilePath to specify where
    pending audit logs should be persisted.
    
    Fixes #15394
    Updates tailscale/corp#26435
    Updates tailscale/corp#27012
    
    Signed-off-by: Nick Khyl <nickk@tailscale.com>

--- ipn/auditlog/auditlog.go
+++ ipn/auditlog/auditlog.go
@@ -112,7 +112,7 @@ func NewLogger(opts Opts) *Logger {
 
 	al := &Logger{
 		retryLimit:  opts.RetryLimit,
-		logf:        logger.WithPrefix(opts.Logf, "auditlog: "),
+		logf:        opts.Logf,
 		store:       opts.Store,
 		flusher:     make(chan struct{}, 1),
 		done:        make(chan struct{}),
@@ -138,7 +138,9 @@ func (al *Logger) FlushAndStop(ctx context.Context) {
 func (al *Logger) SetProfileID(profileID ipn.ProfileID) error {
 	al.mu.Lock()
 	defer al.mu.Unlock()
-	if al.profileID != "" {
+	// It's not an error to call SetProfileID more than once
+	// with the same [ipn.ProfileID].
+	if al.profileID != "" && al.profileID != profileID {
 		return errors.New("profileID already set")
 	}
 
